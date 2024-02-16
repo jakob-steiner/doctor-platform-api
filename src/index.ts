f662960cf65550b686983c99b767fac21c88868e
DichterDev <david.dr.ruescher@outlook.com>
2024-02-16 14:56:49 +0100

    fixed POST /add and added GET /users

--- src/index.ts
+++ src/index.ts
@@ -13,16 +13,30 @@ app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
 });
 
-app.post("/add", (req: Request, res: Response) => {
-    const doctor = prisma.user.create({
+app.post("/add", async (req: Request, res: Response) => {
+    const doctor = await prisma.user.create({
         data: {
             name: "Dr. Strange",
             email: "drstrange@gmail.com",
             password: "123456",
             adress: "New York",
         }
-    })
-})
+    });
+
+    res.send({
+      'status': 200,
+      'user': doctor
+    });
+});
+
+app.get("/users", async(req: Request, res:Response) => {
+  const users = await prisma.user.findMany();
+
+  res.send({
+    'status': 200,
+    'users': users
+  });
+});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
